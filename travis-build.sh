269df09d363b34c1bd56657ed331fe2bea013a94
xjx00 <1504635269@qq.com>
2017-08-12 23:21:55 +0800

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -19,7 +19,7 @@ export BUILD_NR="$(date '+%Y%m%d-%H%M%S')"
 echo "BUILD_NR=$BUILD_NR"
 
 # run build
-export DEFCONFIG=rpi_defconfig
+export DEFCONFIG=bcm2835_defconfig
 docker-compose build
 docker-compose run builder
 
